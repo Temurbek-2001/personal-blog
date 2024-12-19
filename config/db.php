e12eec9d62194fa29f48e418ae39676d3d0c7810
Temurbek-2001 <mirzaliyevtemur@gmail.com>
2024-12-19 21:03:34 +0500

    SCRUM-9: Create migration for blog tables with admin, posts, categories, and post images

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=univer',
     'username' => 'root',
-    'password' => '',
+    'password' => 'newpassword',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
